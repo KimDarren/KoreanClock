c619898718a1837aa79a9597119281b5e864b86a
KimDarren <korean.darren@gmail.com>
2015-05-04 03:29:40 +0900

    Add data utility class

---
+++ KoreanClock/KoreanClock/Utility/KCDataUtility.h
@@ -0,0 +1,13 @@
+//
+//  KCDataUtility.h
+//  KoreanClock
+//
+//  Created by 1001246 on 2015. 5. 4..
+//  Copyright (c) 2015년 USAIU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KCDataUtility : NSObject
+
+@end
---
+++ KoreanClock/KoreanClock/Utility/KCDataUtility.m
@@ -0,0 +1,13 @@
+//
+//  KCDataUtility.m
+//  KoreanClock
+//
+//  Created by 1001246 on 2015. 5. 4..
+//  Copyright (c) 2015년 USAIU. All rights reserved.
+//
+
+#import "KCDataUtility.h"
+
+@implementation KCDataUtility
+
+@end
