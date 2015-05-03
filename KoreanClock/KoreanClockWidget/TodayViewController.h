ca4944a25473ddef5be3213ab274d9018879af52
KimDarren <korean.darren@gmail.com>
2015-05-04 06:43:43 +0900

    Add Tody Extension Target

---
+++ KoreanClock/KoreanClockWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  KoreanClockWidget
+//
+//  Created by 1001246 on 2015. 5. 4..
+//  Copyright (c) 2015년 USAIU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
