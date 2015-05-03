ab00a3e97a359ba18891b9490ed95abf1097361f
KimDarren <korean.darren@gmail.com>
2015-05-04 03:00:08 +0900

    Create project

---
+++ KoreanClock/KoreanClock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KoreanClock
+//
+//  Created by 1001246 on 2015. 5. 4..
+//  Copyright (c) 2015년 USAIU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KoreanClock/KoreanClock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KoreanClock
+//
+//  Created by 1001246 on 2015. 5. 4..
+//  Copyright (c) 2015년 USAIU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
