901a3d8f9c97ff94adc2eddbd2d4b7fe222ffa23
KimDarren <korean.darren@gmail.com>
2015-05-04 03:28:02 +0900

    Add KCCharacterView

---
+++ KoreanClock/KoreanClock/KCCharacterView/KCCharacterView.m
@@ -0,0 +1,21 @@
+//
+//  KCCharacterView.m
+//  KoreanClock
+//
+//  Created by 1001246 on 2015. 5. 4..
+//  Copyright (c) 2015년 USAIU. All rights reserved.
+//
+
+#import "KCCharacterView.h"
+
+@implementation KCCharacterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
